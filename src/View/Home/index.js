da4cc7a1bd5110c46682f653d9a76ad5321e3879
jhumdas <dasjhuma0099@gmail.com>
2023-07-20 19:30:00 +0530

    up

--- src/View/Home/index.js
+++ src/View/Home/index.js
@@ -19,69 +19,35 @@ export default function index() {
   return (
     <>
       <DigitalMoney />
-      <Transaction />
-      <Recharge />
-      <section className='zigzag FirstStep' style={{ backgroundImage: `url('${b}')`, position: 'relative' }}>
-        <div className='cust-container'>
-          <div className='row' style={{ alignItems: "center" }}>
-            <div className='col-lg-6 col-md-6 col-12'>
-              <div className='left'>
-                <img src={a} />
-              </div>
-            </div>
-            <div className='col-lg-6 col-md-6 col-12'>
-              <div className='right'>
-                <h3>You can easily buy & sell your products in this marketplace</h3>
-                <ul>
-                  <li>
-                    Lorem Ipsum is simply dummy
-                    <span>text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</span>
-                  </li>
-                  <li>
-                    Lorem Ipsum is simply dummy
-                    <span>text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</span>
-                  </li>
-                  <li>
-                    Lorem Ipsum is simply dummy
-                    <span>text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</span>
-                  </li>
-                  <a href="#" className='exploreBtn'>Explore</a>
-                </ul>
-
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
       <section className='zigzag' style={{ background: "#F7F7F7" }}>
         <div className='cust-container'>
           <div className='row' style={{ alignItems: "center" }}>
-            <div className='col-lg-6 col-md-6 col-12 orderTwo'>
+            <div className='col-lg-7 col-md-7 col-12 orderTwo'>
               <div className='right'>
                 <h3>The Power of The Bushido SuperAPP.
-</h3>
+                </h3>
                 <ul>
                   <li>
-                  Get involved, showcase or join communities and Projects 
+                    Get involved, showcase or join communities and Projects
 
-                    <span>text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</span>
+
                   </li>
                   <li>
-                  Contribute to growth content that helps others prosper and grow
+                    Contribute to growth content that helps others prosper and grow
 
-                    <span>text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</span>
+
                   </li>
                   <li>
-                  Earn Loyalty points and Donate to projects of your choice 
+                    Earn Loyalty points and Donate to projects of your choice
 
-                    <span>text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</span>
+
                   </li>
                   <a href="#" className='exploreBtn'>Explore</a>
                 </ul>
 
               </div>
             </div>
-            <div className='col-lg-6 col-md-6 col-12 orderOne'>
+            <div className='col-lg-5 col-md-5 col-12 orderOne'>
               <div className='left'>
                 <img src={c} />
               </div>
@@ -90,7 +56,37 @@ export default function index() {
           </div>
         </div>
       </section>
-      <section className='zigzag'>
+      <Transaction />
+      <Recharge />
+      <section className='zigzag FirstStep' style={{ backgroundImage: `url('${b}')`, position: 'relative' }}>
+        <div className='cust-container'>
+          <div className='row' style={{ alignItems: "center" }}>
+            <div className='col-lg-5 col-md-5 col-12'>
+              <div className='left'>
+                <img src={a} />
+              </div>
+            </div>
+            <div className='col-lg-7 col-md-7 col-12'>
+              <div className='right'>
+                <h3>You can easily buy & sell your products in this marketplace</h3>
+                <ul>
+                  <li>
+                    Increase on-site conversions with social galleries
+
+                    <span>Bushido helps businesses to showcase, market and sell – we help them develop affiliate revenues with our innovative conversion technologies. Margins are shared with everyone in their networks, users can donate to sustainable projects or cash out their earnings.
+                    </span>
+                  </li>
+
+                  <a href="#" className='exploreBtn'>Explore</a>
+                </ul>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='zigzag donation'>
         <div className='cust-container'>
           <div className='row' style={{ alignItems: "center" }}>
             <div className='col-lg-6 col-md-6 col-12'>
@@ -101,18 +97,43 @@ export default function index() {
             <div className='col-lg-6 col-md-6 col-12'>
               <div className='right'>
                 <h3>For Humanity purpose Donation & Charity</h3>
+                <p className='mb-2'>A digital platform where the providers of Community, Social & impact investment opportunities can meet those that are interested in Community, Social & Impact Investing.
+                  </p>
                 <ul>
+              
                   <li>
-                    Lorem Ipsum is simply dummy
-                    <span>text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</span>
+
+                    <span>Augment “matched funding” and sponsorship opportunities.</span>
                   </li>
                   <li>
-                    Lorem Ipsum is simply dummy
-                    <span>text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</span>
+
+                    <span>Create learning programmes to promote social entrepreneurship. </span>
                   </li>
                   <li>
-                    Lorem Ipsum is simply dummy
-                    <span>text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</span>
+
+                    <span>Seek grant funding and sponsorship for both Bushido, businesses and/or other entities
+                    </span>
+                  </li>
+                  <li>
+
+                    <span>Provide startup business advice to those within our community.
+                    </span>
+                  </li>
+                  <li>
+
+                    <span>Create fundraising campaigns, stage events and provide training and resources.
+                    </span>
+                  </li>
+                  <li>
+
+                    <span>Foster creative content development.
+                    </span>
+                  </li>
+                  <li>
+
+                    <span>Financing and delivering affordable housing projects.
+
+                    </span>
                   </li>
                   <a href="#" className='exploreBtn'>Explore</a>
                 </ul>
@@ -131,16 +152,25 @@ export default function index() {
                 <h3>Manage Your selling item to be more organised</h3>
                 <ul>
                   <li>
-                    Lorem Ipsum is simply dummy
-                    <span>text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</span>
+                  Investing in Transformative Change through Community, Social & Impact Investment. 
+
+                   
                   </li>
                   <li>
-                    Lorem Ipsum is simply dummy
-                    <span>text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</span>
+                  Community – use our platform to create online virtual communities and talks / showcases 
+
+                   
                   </li>
                   <li>
-                    Lorem Ipsum is simply dummy
-                    <span>text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</span>
+                  Create new social and impact investing initiatives
+
+                  
+                  </li>
+                  <li>
+                  Be part of the one of the biggest personal and business growth platforms which rewards you for making a difference
+
+
+                  
                   </li>
                   <a href="#" className='exploreBtn'>Explore</a>
                 </ul>
@@ -155,7 +185,7 @@ export default function index() {
           </div>
         </div>
       </section>
-      <section className='zigzag'>
+      {/* <section className='zigzag'>
         <div className='cust-container'>
           <div className='row' style={{ alignItems: "center" }}>
             <div className='col-lg-6 col-md-6 col-12'>
@@ -187,7 +217,7 @@ export default function index() {
 
           </div>
         </div>
-      </section>
+      </section> */}
       {/* <section className='zigzag LastStep'>
         <div className='cust-container'>
           <div className='row' style={{ alignItems: "center" }}>
